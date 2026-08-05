0f81c33d258c21b4fce1c01294443c272a6647ee
MillenTec <MillenTec@outlook.com>
2026-08-05 15:57:49 +0800

    feat(Icon): 更改了一些 Filled Icon 的分层逻辑

--- src/uikitMain/src/commonMain/kotlin/com/millentec/compose/uikit/icons/fluenticons/filled/dp20/Delete.kt
+++ src/uikitMain/src/commonMain/kotlin/com/millentec/compose/uikit/icons/fluenticons/filled/dp20/Delete.kt
@@ -1,5 +1,7 @@
 ﻿package com.millentec.compose.uikit.icons.fluenticons.filled.dp20
 
+
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -7,62 +9,91 @@ import androidx.compose.ui.graphics.vector.path
 import androidx.compose.ui.unit.dp
 import com.millentec.compose.uikit.icons.fluenticons.FluentIcons
 
-val FluentIcons.Filled.Delete: ImageVector
-    get() {
-        if (_Delete != null) {
-            return _Delete!!
+fun FluentIcons.Filled.delete(
+    primary: Brush,
+    secondary: Brush
+): ImageVector {
+    return ImageVector.Builder(
+        name = "Delete",
+        defaultWidth = 20.dp,
+        defaultHeight = 20.dp,
+        viewportWidth = 20f,
+        viewportHeight = 20f
+    ).apply {
+        path(fill = secondary) {
+            moveTo(17.5f, 4f)
+            horizontalLineToRelative(-5f)
+            curveToRelative(0f, -1.38f, -1.12f, -2.5f, -2.5f, -2.5f)
+            reflectiveCurveToRelative(-2.5f, 1.12f, -2.5f, 2.5f)
+            horizontalLineTo(2.5f)
+            curveToRelative(-0.28f, 0f, -0.5f, 0.22f, -0.5f, 0.5f)
+            reflectiveCurveToRelative(0.22f, 0.5f, 0.5f, 0.5f)
+            horizontalLineToRelative(1.05f)
+            lineToRelative(1.19f, 10.34f)
+            curveToRelative(0.17f, 1.51f, 1.46f, 2.66f, 2.98f, 2.66f)
+            horizontalLineToRelative(4.54f)
+            curveToRelative(1.52f, 0f, 2.81f, -1.14f, 2.98f, -2.66f)
+            lineToRelative(1.19f, -10.34f)
+            horizontalLineToRelative(1.05f)
+            curveToRelative(0.28f, 0f, 0.5f, -0.22f, 0.5f, -0.5f)
+            reflectiveCurveToRelative(-0.22f, -0.5f, -0.5f, -0.5f)
+            close()
+            moveTo(10f, 2.5f)
+            curveToRelative(0.83f, 0f, 1.5f, 0.67f, 1.5f, 1.5f)
+            horizontalLineToRelative(-3f)
+            curveToRelative(0f, -0.83f, 0.67f, -1.5f, 1.5f, -1.5f)
+            close()
+            moveTo(9f, 14f)
+            curveToRelative(0f, 0.28f, -0.22f, 0.5f, -0.5f, 0.5f)
+            reflectiveCurveToRelative(-0.5f, -0.22f, -0.5f, -0.5f)
+            verticalLineToRelative(-6f)
+            curveToRelative(0f, -0.28f, 0.22f, -0.5f, 0.5f, -0.5f)
+            reflectiveCurveToRelative(0.5f, 0.22f, 0.5f, 0.5f)
+            verticalLineToRelative(6f)
+            close()
+            moveTo(12f, 14f)
+            curveToRelative(0f, 0.28f, -0.22f, 0.5f, -0.5f, 0.5f)
+            reflectiveCurveToRelative(-0.5f, -0.22f, -0.5f, -0.5f)
+            verticalLineToRelative(-6f)
+            curveToRelative(0f, -0.28f, 0.22f, -0.5f, 0.5f, -0.5f)
+            reflectiveCurveToRelative(0.5f, 0.22f, 0.5f, 0.5f)
+            verticalLineToRelative(6f)
+            close()
         }
-        _Delete = ImageVector.Builder(
-            name = "Delete",
-            defaultWidth = 20.dp,
-            defaultHeight = 20.dp,
-            viewportWidth = 20f,
-            viewportHeight = 20f
-        ).apply {
-            path(fill = SolidColor(Color(0xFF1D1D1F))) {
-                moveTo(8.5f, 4f)
-                horizontalLineTo(11.5f)
-                curveTo(11.5f, 3.172f, 10.828f, 2.5f, 10f, 2.5f)
-                curveTo(9.172f, 2.5f, 8.5f, 3.172f, 8.5f, 4f)
-                close()
-                moveTo(7.5f, 4f)
-                curveTo(7.5f, 2.619f, 8.619f, 1.5f, 10f, 1.5f)
-                curveTo(11.381f, 1.5f, 12.5f, 2.619f, 12.5f, 4f)
-                horizontalLineTo(17.5f)
-                curveTo(17.776f, 4f, 18f, 4.224f, 18f, 4.5f)
-                curveTo(18f, 4.776f, 17.776f, 5f, 17.5f, 5f)
-                horizontalLineTo(16.446f)
-                lineTo(15.252f, 15.344f)
-                curveTo(15.077f, 16.858f, 13.796f, 18f, 12.272f, 18f)
-                horizontalLineTo(7.728f)
-                curveTo(6.204f, 18f, 4.923f, 16.858f, 4.748f, 15.344f)
-                lineTo(3.554f, 5f)
-                horizontalLineTo(2.5f)
-                curveTo(2.224f, 5f, 2f, 4.776f, 2f, 4.5f)
-                curveTo(2f, 4.224f, 2.224f, 4f, 2.5f, 4f)
-                horizontalLineTo(7.5f)
-                close()
-                moveTo(9f, 8f)
-                curveTo(9f, 7.724f, 8.776f, 7.5f, 8.5f, 7.5f)
-                curveTo(8.224f, 7.5f, 8f, 7.724f, 8f, 8f)
-                verticalLineTo(14f)
-                curveTo(8f, 14.276f, 8.224f, 14.5f, 8.5f, 14.5f)
-                curveTo(8.776f, 14.5f, 9f, 14.276f, 9f, 14f)
-                verticalLineTo(8f)
-                close()
-                moveTo(11.5f, 7.5f)
-                curveTo(11.224f, 7.5f, 11f, 7.724f, 11f, 8f)
-                verticalLineTo(14f)
-                curveTo(11f, 14.276f, 11.224f, 14.5f, 11.5f, 14.5f)
-                curveTo(11.776f, 14.5f, 12f, 14.276f, 12f, 14f)
-                verticalLineTo(8f)
-                curveTo(12f, 7.724f, 11.776f, 7.5f, 11.5f, 7.5f)
-                close()
-            }
-        }.build()
+        path(fill = primary) {
+            moveTo(9f, 8f)
+            curveToRelative(0f, -0.28f, -0.22f, -0.5f, -0.5f, -0.5f)
+            reflectiveCurveToRelative(-0.5f, 0.22f, -0.5f, 0.5f)
+            verticalLineToRelative(6f)
+            curveToRelative(0f, 0.28f, 0.22f, 0.5f, 0.5f, 0.5f)
+            reflectiveCurveToRelative(0.5f, -0.22f, 0.5f, -0.5f)
+            verticalLineToRelative(-6f)
+            close()
+            moveTo(11.5f, 7.5f)
+            curveToRelative(-0.28f, 0f, -0.5f, 0.22f, -0.5f, 0.5f)
+            verticalLineToRelative(6f)
+            curveToRelative(0f, 0.28f, 0.22f, 0.5f, 0.5f, 0.5f)
+            reflectiveCurveToRelative(0.5f, -0.22f, 0.5f, -0.5f)
+            verticalLineToRelative(-6f)
+            curveToRelative(0f, -0.28f, -0.22f, -0.5f, -0.5f, -0.5f)
+            close()
+        }
+    }.build()
+}
 
-        return _Delete!!
-    }
+fun FluentIcons.Filled.delete(): ImageVector {
+    return delete(
+        SolidColor(Color(0x001D1D1F)),
+        SolidColor(Color(0xFF1D1D1F))
+    )
+}
 
-@Suppress("ObjectPropertyName")
-private var _Delete: ImageVector? = null
+fun FluentIcons.Filled.delete(
+    color: Color,
+    layered: Boolean = false
+): ImageVector {
+    return delete(
+        if (layered) SolidColor(color) else SolidColor(color.copy(0f)),
+        if (layered) SolidColor(color.copy(0.3f)) else SolidColor(color)
+    )
+}
--- src/uikitMain/src/commonMain/kotlin/com/millentec/compose/uikit/icons/fluenticons/filled/dp20/ZoomIn.kt
+++ src/uikitMain/src/commonMain/kotlin/com/millentec/compose/uikit/icons/fluenticons/filled/dp20/ZoomIn.kt
@@ -1,5 +1,7 @@
 ﻿package com.millentec.compose.uikit.icons.fluenticons.filled.dp20
 
+
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -7,54 +9,85 @@ import androidx.compose.ui.graphics.vector.path
 import androidx.compose.ui.unit.dp
 import com.millentec.compose.uikit.icons.fluenticons.FluentIcons
 
-val FluentIcons.Filled.ZoomIn: ImageVector
-    get() {
-        if (_ZoomIn != null) {
-            return _ZoomIn!!
+fun FluentIcons.Filled.zoomIn(
+    primary: Brush,
+    secondary: Brush
+): ImageVector {
+    return ImageVector.Builder(
+        name = "ZoomIn",
+        defaultWidth = 20.dp,
+        defaultHeight = 20.dp,
+        viewportWidth = 20f,
+        viewportHeight = 20f
+    ).apply {
+        path(fill = secondary) {
+            moveTo(16.85f, 16.15f)
+            lineToRelative(-3.42f, -3.42f)
+            curveToRelative(0.97f, -1.14f, 1.56f, -2.61f, 1.56f, -4.23f)
+            curveToRelative(0f, -3.59f, -2.91f, -6.5f, -6.5f, -6.5f)
+            reflectiveCurveToRelative(-6.5f, 2.91f, -6.5f, 6.5f)
+            reflectiveCurveToRelative(2.91f, 6.5f, 6.5f, 6.5f)
+            curveToRelative(1.61f, 0f, 3.09f, -0.59f, 4.23f, -1.56f)
+            lineToRelative(3.42f, 3.42f)
+            lineToRelative(0.07f, 0.06f)
+            curveToRelative(0.19f, 0.14f, 0.46f, 0.12f, 0.64f, -0.06f)
+            curveToRelative(0.2f, -0.2f, 0.2f, -0.51f, 0f, -0.71f)
+            close()
+            moveTo(11f, 9f)
+            horizontalLineToRelative(-2f)
+            verticalLineToRelative(2f)
+            curveToRelative(0f, 0.28f, -0.22f, 0.5f, -0.5f, 0.5f)
+            reflectiveCurveToRelative(-0.5f, -0.22f, -0.5f, -0.5f)
+            verticalLineToRelative(-2f)
+            horizontalLineToRelative(-2f)
+            curveToRelative(-0.28f, 0f, -0.5f, -0.22f, -0.5f, -0.5f)
+            reflectiveCurveToRelative(0.22f, -0.5f, 0.5f, -0.5f)
+            horizontalLineToRelative(2f)
+            verticalLineToRelative(-2f)
+            curveToRelative(0f, -0.28f, 0.22f, -0.5f, 0.5f, -0.5f)
+            reflectiveCurveToRelative(0.5f, 0.22f, 0.5f, 0.5f)
+            verticalLineToRelative(2f)
+            horizontalLineToRelative(2f)
+            curveToRelative(0.28f, 0f, 0.5f, 0.22f, 0.5f, 0.5f)
+            reflectiveCurveToRelative(-0.22f, 0.5f, -0.5f, 0.5f)
+            close()
         }
-        _ZoomIn = ImageVector.Builder(
-            name = "ZoomIn",
-            defaultWidth = 20.dp,
-            defaultHeight = 20.dp,
-            viewportWidth = 20f,
-            viewportHeight = 20f
-        ).apply {
-            path(fill = SolidColor(Color(0xFF1D1D1F))) {
-                moveTo(8.5f, 2f)
-                curveTo(12.09f, 2f, 15f, 4.91f, 15f, 8.5f)
-                curveTo(15f, 10.115f, 14.411f, 11.593f, 13.436f, 12.729f)
-                lineTo(16.854f, 16.146f)
-                curveTo(17.049f, 16.342f, 17.049f, 16.658f, 16.854f, 16.854f)
-                curveTo(16.68f, 17.027f, 16.411f, 17.046f, 16.216f, 16.911f)
-                lineTo(16.146f, 16.854f)
-                lineTo(12.729f, 13.436f)
-                curveTo(11.593f, 14.411f, 10.115f, 15f, 8.5f, 15f)
-                curveTo(4.91f, 15f, 2f, 12.09f, 2f, 8.5f)
-                curveTo(2f, 4.91f, 4.91f, 2f, 8.5f, 2f)
-                close()
-                moveTo(8.5f, 5.5f)
-                curveTo(8.224f, 5.5f, 8f, 5.724f, 8f, 6f)
-                verticalLineTo(8f)
-                horizontalLineTo(6f)
-                curveTo(5.724f, 8f, 5.5f, 8.224f, 5.5f, 8.5f)
-                curveTo(5.5f, 8.776f, 5.724f, 9f, 6f, 9f)
-                horizontalLineTo(8f)
-                verticalLineTo(11f)
-                curveTo(8f, 11.276f, 8.224f, 11.5f, 8.5f, 11.5f)
-                curveTo(8.776f, 11.5f, 9f, 11.276f, 9f, 11f)
-                verticalLineTo(9f)
-                horizontalLineTo(11f)
-                curveTo(11.276f, 9f, 11.5f, 8.776f, 11.5f, 8.5f)
-                curveTo(11.5f, 8.224f, 11.276f, 8f, 11f, 8f)
-                horizontalLineTo(9f)
-                verticalLineTo(6f)
-                curveTo(9f, 5.724f, 8.776f, 5.5f, 8.5f, 5.5f)
-                close()
-            }
-        }.build()
+        path(fill = primary) {
+            moveTo(8.5f, 5.5f)
+            curveToRelative(-0.28f, 0f, -0.5f, 0.22f, -0.5f, 0.5f)
+            verticalLineToRelative(2f)
+            horizontalLineToRelative(-2f)
+            curveToRelative(-0.28f, 0f, -0.5f, 0.22f, -0.5f, 0.5f)
+            reflectiveCurveToRelative(0.22f, 0.5f, 0.5f, 0.5f)
+            horizontalLineToRelative(2f)
+            verticalLineToRelative(2f)
+            curveToRelative(0f, 0.28f, 0.22f, 0.5f, 0.5f, 0.5f)
+            reflectiveCurveToRelative(0.5f, -0.22f, 0.5f, -0.5f)
+            verticalLineToRelative(-2f)
+            horizontalLineToRelative(2f)
+            curveToRelative(0.28f, 0f, 0.5f, -0.22f, 0.5f, -0.5f)
+            reflectiveCurveToRelative(-0.22f, -0.5f, -0.5f, -0.5f)
+            horizontalLineToRelative(-2f)
+            verticalLineToRelative(-2f)
+            curveToRelative(0f, -0.28f, -0.22f, -0.5f, -0.5f, -0.5f)
+            close()
+        }
+    }.build()
+}
 
-        return _ZoomIn!!
-    }
+fun FluentIcons.Filled.zoomIn(): ImageVector {
+    return zoomIn(
+        SolidColor(Color(0x001D1D1F)),
+        SolidColor(Color(0xFF1D1D1F))
+    )
+}
 
-@Suppress("ObjectPropertyName")
-private var _ZoomIn: ImageVector? = null
+fun FluentIcons.Filled.zoomIn(
+    color: Color,
+    layered: Boolean = false
+): ImageVector {
+    return zoomIn(
+        if (layered) SolidColor(color) else SolidColor(color.copy(0f)),
+        if (layered) SolidColor(color.copy(0.3f)) else SolidColor(color)
+    )
+}
