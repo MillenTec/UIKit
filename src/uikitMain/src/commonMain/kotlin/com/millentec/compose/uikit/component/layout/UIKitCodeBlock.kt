a05ef568d6b07b19fb9bd77efca56810dff8e181
MillenTec <MillenTec@outlook.com>
2026-08-08 16:57:49 +0800

    feat(Lyout): 添加了 UIKitCodeViewer 控件 - 相比于基础 CodeBlock, 它是一个完整控件, 支持显示行号

--- src/uikitMain/src/commonMain/kotlin/com/millentec/compose/uikit/component/layout/UIKitCodeBlock.kt
+++ src/uikitMain/src/commonMain/kotlin/com/millentec/compose/uikit/component/layout/UIKitCodeBlock.kt
@@ -1,41 +1,58 @@
 ﻿package com.millentec.compose.uikit.component.layout
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.millentec.compose.uikit.foundation.UIKitCodeBlockColors
 import com.millentec.compose.uikit.foundation.UIKitCodeBlockConfig
-import com.millentec.compose.uikit.theme.getUIKitLayout
-import com.millentec.compose.uikit.theme.getUIKitTypography
+import com.millentec.compose.uikit.theme.*
 
 @Composable
 @Preview
 private fun Preview() {
-    UIKitCodeBlock(
-        config = UIKitCodeBlockConfig.kotlin(UIKitCodeBlockColors.IntelliJ)
-    ) {
-        """
+    UIKitThemeHost(UIKitTheme.getDark()) {
+        UIKitCodeViewer(
+            config = UIKitCodeBlockConfig.kotlin(UIKitCodeBlockColors.IntelliJ),
+        ) {
+            """
         val num = 123f
         println("Hello World!")
         println(num)
         
         @Composable
         fun App() {
-            // Some Notes
+            // Some Notes Some Notes Some Notes Some Notes Some Notes Some Notes 
             /* Some Notes */
             /**
              Some Docs
              */
         }
         """.trimIndent()
+        }
     }
 }
 
@@ -46,6 +63,7 @@ fun UIKitCodeBlock(
     style: TextStyle = getUIKitTypography().body,
     fontFamily: FontFamily = FontFamily.Monospace,
     contentPadding: PaddingValues = PaddingValues(getUIKitLayout().basicSpacing),
+    onTextLayout: ((TextLayoutResult) -> Unit)? = null,
     content: @Composable () -> String
 ) {
     val content = content()
@@ -76,7 +94,129 @@ fun UIKitCodeBlock(
             text = annotatedString,
             style = style,
             fontFamily = fontFamily,
-            color = config.colors.primaryText
+            color = config.colors.primaryText,
+            onTextLayout = {
+                onTextLayout?.invoke(it)
+            }
         )
+    }
+}
+
+@Composable
+fun UIKitCodeViewer(
+    modifier: Modifier = Modifier,
+    background: Color = getUIKitColors().contentFillColorSecondaryBrush,
+    divider: Color? = getUIKitColors().lineFillColorPrimaryBrush,
+    cornerRadius: Dp = getUIKitShapes().regularRounded,
+    lineNumberEnabled: Boolean = true,
+    warpEnabled: Boolean = false,
+    config: UIKitCodeBlockConfig = UIKitCodeBlockConfig.text(),
+    textStyle: TextStyle = getUIKitTypography().body,
+    fontFamily: FontFamily = FontFamily.Monospace,
+    content: @Composable () -> String
+) {
+    Column(
+        modifier = modifier
+            .clip(RoundedCornerShape(cornerRadius))
+            .background(background)
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(getUIKitLayout().basicSpacing)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Text(
+                text = config.name,
+                style = getUIKitTypography().body,
+                color = getUIKitColors().textFillColorSecondaryBrush
+            )
+        }
+
+        divider?.let {
+            HorizontalDivider(
+                color = divider,
+                thickness = 1.dp
+            )
+        }
+
+        Row(
+            modifier = Modifier
+                .height(IntrinsicSize.Max)
+        ) {
+            val lineHeight = textStyle.lineHeight.value.dp
+            val lineCount = remember { mutableStateOf(0) }
+
+            AnimatedVisibility(
+                visible = lineNumberEnabled,
+                enter = slideInHorizontally(
+                    animationSpec = tween(
+                        getUIKitAnimate().motionRegularDurationMillis,
+                        easing = FastOutSlowInEasing
+                    )
+                ) {
+                    -it
+                },
+                exit = slideOutHorizontally(
+                    animationSpec = tween(
+                        getUIKitAnimate().motionRegularDurationMillis,
+                        easing = FastOutSlowInEasing
+                    )
+                ) {
+                    -it
+                }
+            ) {
+                Row {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .padding(getUIKitLayout().basicSpacing)
+                    ) {
+                        repeat(lineCount.value) {
+                            Box(
+                                modifier = Modifier
+                                    .height(lineHeight),
+                                contentAlignment = Alignment.Center,
+                            ) {
+                                Text(
+                                    text = (it + 1).toString(),
+                                    style = getUIKitTypography().footnote,
+                                    color = getUIKitColors().textFillColorSecondaryBrush
+                                )
+                            }
+                        }
+                    }
+
+                    divider?.let {
+                        VerticalDivider(
+                            color = divider,
+                            thickness = 1.dp
+                        )
+                    }
+                }
+            }
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(config.colors.primaryBackground)
+            ) {
+                UIKitCodeBlock(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .then(if (!warpEnabled) {
+                            Modifier.horizontalScroll(rememberScrollState())
+                        } else Modifier),
+                    config = config,
+                    style = textStyle,
+                    fontFamily = fontFamily,
+                    contentPadding = PaddingValues(getUIKitLayout().basicSpacing),
+                    content = content,
+                    onTextLayout = {
+                        lineCount.value = it.lineCount
+                    }
+                )
+            }
+        }
     }
 }
